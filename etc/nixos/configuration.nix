d281a1a64066ac47a91c666da278e7da64740c8a
Dmitry Tomp <dmitrytomp@gmail.com>
2017-09-24 15:27:56 +0300

    Initial commit

---
+++ etc/nixos/configuration.nix
@@ -0,0 +1,181 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  addSkb = {stdenv, fetchFromGitHub, xlibs}:
+    stdenv.mkDerivation rec {
+      name = "skb";
+    
+      src = fetchFromGitHub {
+        owner = "polachok";
+        repo = "skb";
+        rev = "38aec5ccac505e2e95af50f1552e6b80715dfff6";
+        sha256 = "1lwwvsgwh05q33k1s2brh1da9gznrfhazb2k72jhxkvdwi4c99sw";
+      }; 
+      buildInputs = [ xlibs.libX11 ];
+      CFLAGS = "-w";
+    
+      prePatch = ''sed -i "s@/usr@$out@" config.mk'';
+      patches = [ ./skb-cflags.patch ];
+    };
+  addTompebar = {stdenv, mkDerivation, fetchFromGitHub, base, directory, network, process}:
+    mkDerivation {
+      pname = "tompebar";
+      version = "0.1.0.0";
+      src = fetchFromGitHub {
+        owner = "dimatomp";
+        repo = "tompebar";
+        rev = "f940042aa4ffbd1e8b666d4669ebb648fd166a18";
+        sha256 = "1c71iygnssib82fpyajgx7sfk70hqiivxvykbddmbwikr7fdk7kp";
+      };
+      isLibrary = false;
+      isExecutable = true;
+      executableHaskellDepends = [ base directory network process ];
+      description = "An enhancement for the bspwm desktop environment. Divides the desktop list into workspaces for better multitasking.";
+      license = stdenv.lib.licenses.free;
+    };
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.supportedFilesystems = [ "ntfs-3g" ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [ "ru_RU.UTF-8/UTF-8" "ru_RU.KOI8-R/KOI8-R" "ru_RU/ISO-8859-5" "en_US.UTF-8/UTF-8" ];
+    inputMethod.enabled = "uim";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Moscow";
+
+  nixpkgs.config.haskellPackageOverrides = self: super: {
+    tompebar = self.callPackage addTompebar {};
+  };
+
+  nixpkgs.config.packageOverrides = pkgs: rec {
+    skb = with pkgs; callPackage addSkb {};
+    #bar-xft = pkgs.bar-xft.overrideDerivation (old: {
+    #  name = "bar-xft-git-2016-11-07";
+    #  src = pkgs.fetchFromGitHub {
+    #    owner = "krypt-n";
+    #    repo = "bar";
+    #    rev = "043ad4757cc079666f50212ee0a2ef0729ecac6b";
+    #    sha256 = "0plarlqdc54xlhz4np1xr231xs4lkhgaphrqzycljav35wskpsc8";
+    #  };
+    #});
+    #mariadb-beta = pkgs.mariadb.overrideDerivation (old: { 
+    #  name = "mariadb-10.2.2b";
+    #  version = "10.2.2";
+    #  src = pkgs.fetchurl {
+    #    url    = "https://downloads.mariadb.org/interstitial/mariadb-10.2.2/source/mariadb-10.2.2.tar.gz";
+    #    sha256 = "0fl9dymb3kqsrydxmdkmg0x9pna6ayhvhz0f6g0iylg4g8srxksm";
+    #  };
+    #  cmakeFlags = old.cmakeFlags ++ [ "-DWITH_DEBUG=1" ];
+    #  enableParallelBuilding = true;
+    #  postInstall = ''
+    #    rm "$out"/lib/*.a
+    #    find "''${!outputBin}/bin" -name '*test*' -delete
+    #    rm -r "$out"/{mysql-test,sql-bench,data} # Don't need testing data
+    #    rm "$out"/share/man/man1/mysql-test-run.pl.1
+    #  '';
+    #});
+  };
+ 
+  nixpkgs.config.firefox = {
+    enableGoogleTalkPlugin = true;
+    enableAdobeFlash = true;
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    wget which git htop cifs_utils vim_configurable
+    haskellPackages.tompebar 
+    acpi xtitle bar-xft trayer sakura dmenu skb sselp #i3lock
+    pavucontrol networkmanagerapplet firefox filelight
+  ];
+
+  fonts = {
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    fonts = with pkgs; [
+      corefonts
+      google-fonts
+      dejavu_fonts
+    ];
+  };
+
+  # List services that you want to enable:
+  #services.samba.enable = true;
+  #services.printing.enable = true;
+  services.openssh.enable = true;
+  services.xserver = {
+    enable = true;
+    layout = "us,ru";
+    windowManager = {
+      bspwm.enable = true;
+      default = "bspwm";
+    };
+    xkbOptions = "grp:caps_toggle";
+    displayManager.auto = {
+      enable = true;
+      user = "dimatomp";
+    };
+  };
+  services.mysql = {
+    enable = false;
+    package = pkgs.mariadb;
+  };
+  services.postgresql.enable = false;
+  virtualisation.docker.enable = false;
+  networking.networkmanager.enable = true;
+
+  services.logind.extraConfig = ''
+    RuntimeDirectorySize=2G
+  '';
+
+  programs.zsh.enable = true;
+
+  users.extraUsers.dimatomp = {
+    isNormalUser = true;
+    uid = 1000;
+    shell = "/run/current-system/sw/bin/zsh";
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  environment.shellAliases = {
+    dh = "df -h";
+    ll = "ls -lah";
+  };
+
+  environment.variables = {
+    _JAVA_AWT_WM_NONREPARENTING="1";
+    LOCK_SCREEN = "slimlock";
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.extraUsers.guest = {
+  #   isNormalUser = true;
+  #   uid = 1000;
+  # };
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "17.03";
+  #system.stateVersion = "16.09";
+  nix.buildCores = 0;
+
+}
